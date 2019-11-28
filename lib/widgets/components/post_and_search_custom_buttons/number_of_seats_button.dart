bd6da3336f8dab68d9e424a7392fddaa8d71efb8
festeban26 <festeban26@gmail.com>
2019-11-27 23:28:04 -0500

    Functionality added to number of seats button

--- lib/widgets/components/post_and_search_custom_buttons/number_of_seats_button.dart
+++ lib/widgets/components/post_and_search_custom_buttons/number_of_seats_button.dart
@@ -1,3 +1,5 @@
+import 'package:aventones/res/company_colors.dart';
+import 'package:aventones/res/dimensions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'custom_icon_button.dart';
@@ -12,35 +14,49 @@ class NumberOfSeatsButton extends StatefulWidget {
 }
 
 class _NumberOfSeatsButtonState extends State<NumberOfSeatsButton> {
-  TimeOfDay _selectedTime = TimeOfDay.now();
+  int _selectedNumberOfAvailableSeats = 1;
 
   @override
   Widget build(BuildContext context) {
-    String _formattedTime = _selectedTime.format(context);
-
-    // TODO: implement build
     return CustomIconButton(
       title: "Asientos disponibles",
-      label: _formattedTime,
+      label: _selectedNumberOfAvailableSeats.toString(),
       iconColor: widget.iconColor,
       iconData: Icons.event_seat,
       onTap: () {
         showDialog(
           context: context,
-          builder: (_) => FunkyOverlay(),
+          builder: (_) => SelectNumberOfSeatsDialog(
+            onValueSelected: (int value){
+              setState(() {
+                _selectedNumberOfAvailableSeats = value;
+              });
+            },
+          ),
         );
       },
     );
   }
 }
 
-class FunkyOverlay extends StatefulWidget {
+class SelectNumberOfSeatsDialog extends StatefulWidget {
+  SelectNumberOfSeatsDialog({this.onValueSelected});
+
+  final _DialogCallback onValueSelected;
+
   @override
-  State<StatefulWidget> createState() => FunkyOverlayState();
+  State<StatefulWidget> createState() =>
+      SelectNumberOfSeatsDialogState(onValueSelect: onValueSelected);
 }
 
-class FunkyOverlayState extends State<FunkyOverlay>
+typedef _DialogCallback = void Function(int selectedValue);
+
+class SelectNumberOfSeatsDialogState extends State<SelectNumberOfSeatsDialog>
     with SingleTickerProviderStateMixin {
+  SelectNumberOfSeatsDialogState({this.onValueSelect});
+
+  final _DialogCallback onValueSelect;
+
   AnimationController controller;
   Animation<double> scaleAnimation;
 
@@ -50,8 +66,7 @@ class FunkyOverlayState extends State<FunkyOverlay>
 
     controller =
         AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    scaleAnimation =
-        CurvedAnimation(parent: controller, curve: Curves.ease);
+    scaleAnimation = CurvedAnimation(parent: controller, curve: Curves.ease);
 
     controller.forward();
     controller.addListener(() => setState(() {}));
@@ -63,18 +78,63 @@ class FunkyOverlayState extends State<FunkyOverlay>
       child: Material(
         color: Colors.transparent,
         child: ScaleTransition(
-          scale: scaleAnimation,
-          child: Container(
-            decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16.0))),
-            child: Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Text("Well hello there!"),
-            ),
-          ),
-        ),
+            scale: scaleAnimation,
+            child: Container(
+              decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0))),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text("¿Cuántos asientos disponibles tienes?",
+                        style: TextStyle(
+                            fontSize: Dimensions.modalText_TextSize,
+                            fontWeight: FontWeight.bold)),
+                    SizedBox(height: 8.0),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          color: CompanyColors.customLightGray,
+                          child: FlatButton(
+                            child: Text("1"),
+                            onPressed: () {
+                              onValueSelect(1);
+                              Navigator.pop(context, 1);
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Container(
+                          color: CompanyColors.customLightGray,
+                          child: FlatButton(
+                            child: Text("2"),
+                            onPressed: () {
+                              onValueSelect(2);
+                              Navigator.pop(context, 2);
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Container(
+                          color: CompanyColors.customLightGray,
+                          child: FlatButton(
+                            child: Text("3"),
+                            onPressed: () {
+                              Navigator.pop(context, 3);
+                              onValueSelect(3);
+                            },
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )),
       ),
     );
   }
